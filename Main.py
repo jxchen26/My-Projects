93e866d17721ab0f1f483da2e1e95fe151f1f56e
Jxchen26 <xunj.chen@gmail.com>
2019-12-27 18:26:43 -0800

    added collision

--- Main.py
+++ Main.py
@@ -7,8 +7,8 @@ class Tetris(t.Turtle):
 	#game constants
 	screen_width = 800
 	screen_height = 960
-	board_w = 10
-	board_h = 24
+	board_w = 12 # includes borders
+	board_h = 26
 	square_w = int(int(screen_width/2)/board_w)
 	square_h = int(screen_height/board_h)
 	upper_left_corner = {
@@ -18,11 +18,11 @@ class Tetris(t.Turtle):
 	print(upper_left_corner["x"],upper_left_corner["y"])
 	print("the square width: ", square_w, "the sqaure height:",square_h)
 
-	init_xoffset = 120
+	init_xoffset = 99# 3 sqaures to the right 
 
 	def __init__(self):
 		super().__init__() # part of the turtle class 
-		self.board = [0 for i in range(self.board_w * self.board_h)]
+		self.board = self.create_board()
 		self.piece = None
 		self.pointer = {
 		"x":self.upper_left_corner["x"]+self.init_xoffset, 
@@ -30,9 +30,19 @@ class Tetris(t.Turtle):
 		} #upper left corner 
 		print("BOARD:",self.board)
 
-	def create_board():
+	def create_board(self):
 		board = []
-		for i in range(self.board_w*self.board_h)
+		counter = self.board_h
+		for col in range(self.board_h):
+			for row in range(self.board_w):
+				i = col*12 + row
+				if i <= 11 or i%12 ==0 or i==(col+1)*12-1 or i >=300:
+					board.append("%")
+				else:
+					board.append(0)
+		return board
+
+
 
 	def draw_square(self, block):
 
@@ -41,13 +51,13 @@ class Tetris(t.Turtle):
 			self.fillcolor("white")
 		elif block == "#":
 			self.pencolor(190,190,190)
-			self.fillcolor("blue")
+			self.fillcolor("red")
+		elif block == "%":
+			self.pencolor(55,55,55)
+			self.fillcolor(55,55,55)
 		else:
 			self.pencolor(190,190,190)
 			self.fillcolor("blue")
-
-
-
 
 		self.begin_fill()
 		for i in range(2):
@@ -65,12 +75,11 @@ class Tetris(t.Turtle):
 		self.pendown()
 		self.pensize(2)
 
-
 		y = self.upper_left_corner["y"]
 		for s in range(len(self.board)): #10
 			self.draw_square(self.board[s])
 			self.setx(self.xcor()+self.square_w) # increase pen's x position
-			if (s+1)%10 == 0:
+			if (s+1)%12 == 0:
 				# after every row, move pen down 1 row and reset x
 				y -= self.square_h
 				self.penup()
@@ -87,21 +96,12 @@ class Tetris(t.Turtle):
 		x = self.pointer["x"]
 		y= self.pointer["y"] 
 
-
 		for row in range(5):
 			self.penup()
 			self.goto(x,y)
 			self.pendown()
 			for col in range(5):
-				if self.piece.orientation[(row*self.piece.w)+col] == "#":
-					self.fillcolor("red")
-					self.begin_fill()
-					for i in range(2):
-						self.forward(self.square_w)
-						self.right(90)
-						self.forward(self.square_h)
-						self.right(90)
-					self.end_fill()
+				self.draw_square(self.piece.orientation[(row*self.piece.w)+col])
 
 				#move right one square 
 				x += self.square_w
@@ -110,28 +110,67 @@ class Tetris(t.Turtle):
 			x = self.pointer["x"]
 			y -= self.square_h
 
+	def border_collision(self):
+		x_index = int((self.pointer["x"]+400)/self.square_w) 
+		y_index = int(-1*(self.pointer["y"]-480)/self.square_h)
+		board_index = y_index*self.board_w+x_index
+
+		#has to be 2 for loops for i in range of 5 to iterate the tetromino
+		for row in range(5):
+			for col in range(5):
+				if self.piece.orientation[(row*self.piece.w)+col] != "#" and self.board[board_index+col] ==0 : # ignore all empty spot 
+					continue
+				elif self.piece.orientation[(row*self.piece.w)+col] == "#" and self.board[board_index+col] !=0 : # theres collision 
+					print("Collision!")
+					return True 
+			board_index += 12
+
+		return False
+
+
 	def move_down(self):
 		self.pointer["y"] -= self.square_h
+		if self.border_collision():
+			self.pointer["y"] += self.square_h
+			print("collision detected")
+			return 
+		
 
-		if self.pointer["y"] <= -int(self.screen_height/2):
-			self.pointer["y"] = int(self.screen_height/2)
 
 	def left_arrow_press(self):
+		if self.border_collision():
+			print("collision detected")
+			return 
 		self.piece.rotate_left()
 
 	def right_arrow_press(self):
+		if self.border_collision():
+			print("collision detected")
+			return 
 		self.piece.rotate_right()
 
 	def move_up(self):
 		self.pointer["y"]+= self.square_h
+		if self.border_collision():
+			self.pointer["y"]-= self.square_h
+			print("collision detected")
+			return 
+		
 
 	def move_left(self):
-		print("a pressed self.pointer[0]:",self.pointer["x"])
 		self.pointer["x"] -= self.square_w
+		if self.border_collision():
+			print("collision detected")
+			self.pointer["x"] += self.square_w
+			return 
 
 	def move_right(self):
-		print("d pressed self.pointer[0]:",self.pointer["x"])
 		self.pointer["x"] += self.square_w
+		if self.border_collision():
+			self.pointer["x"] -= self.square_w
+			print("collision detected")
+			return 
+		
 		
 	def set_piece(self):
 		x_index = int((self.pointer["x"]+400)/self.square_w) 
@@ -148,7 +187,7 @@ class Tetris(t.Turtle):
 				elif self.piece.orientation[(row*self.piece.w)+col] == "#" and self.board[board_index+col] !=0 : # theres collision 
 					print("Collision!")
 					return 
-			board_index += 10
+			board_index += 12
 			print("updated board index:", board_index)
 
 		board_index = y_index*self.board_w+x_index
@@ -158,7 +197,7 @@ class Tetris(t.Turtle):
 					continue
 				self.board[board_index+col] = "#"
 			 # skip one row on the board 
-			board_index += 10
+			board_index += 12
 
 		print("NEW BOARD:", self.board)
 
